d3c99fb6247d324c7d6b8d12a465cd20cc70dd8e
Sean Morrison <student@epicodus.com>
2014-08-23 18:00:10 -0700

    Add alias for index page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   match('/', {via: :get, to: 'artists#index'})
+  match('/artists', {via: :get, to: 'artists#index'})
   match('/artists/new', {via: :get, to: 'artists#new'})
   match('/artists', {via: :post, to: 'artists#create'})
   match('/artists/:id', {via: :get, to: 'artists#show'})
